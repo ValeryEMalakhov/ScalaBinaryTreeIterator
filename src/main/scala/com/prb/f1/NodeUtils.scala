2720231528dee2a46a66abd381ba655ca685597b
Valery.Malakhov <Valery.Malakhov@sigma.software>
2017-11-14 13:32:50 +0200

    Fix `tailRecTreeTraversal`.

--- src/main/scala/com/prb/f1/NodeUtils.scala
+++ src/main/scala/com/prb/f1/NodeUtils.scala
@@ -35,25 +35,23 @@ object NodeUtils {
         curNode: Option[Node[T]]): Unit = {
 
       curNode match {
-        case Some(cur) =>
-          cur match {
-            // if isLeaf, try to get the parent Node
-            case Node(_, None, None, parent, _) =>
-              tailRecTreeTraversal(visitor, parent)
-
-            // if right Node exist - go right and delete this way from parent Node
-            case Node(_, None, right, _, _) =>
-              right.foreach(r => visitor(r.value))
-              right.foreach(r => r.parent = Some(Node(cur.value, cur.left, None, cur.parent, cur.childCount)))
-              tailRecTreeTraversal(visitor, right)
-
-            // if left Node exist - go left and delete this way from parent Node
-            case Node(_, left, _, _, _) =>
-              left.foreach(l => visitor(l.value))
-              left.foreach(l => l.parent = Some(Node(cur.value, None, cur.right, cur.parent, cur.childCount)))
-              tailRecTreeTraversal(visitor, left)
-          }
         case None =>
+
+        // if isLeaf, try to get the parent Node
+        case Some(Node(_, None, None, parent, _)) =>
+          tailRecTreeTraversal(visitor, parent)
+
+        // if right Node exist - go right and delete this way from parent Node
+        case Some(Node(value, None, right, parent, childCount)) =>
+          right.foreach(r => visitor(r.value))
+          right.foreach(r => r.parent = Some(Node(value, None, None, parent, childCount)))
+          tailRecTreeTraversal(visitor, right)
+
+        // if left Node exist - go left and delete this way from parent Node
+        case Some(Node(value, left, right, parent, childCount)) =>
+          left.foreach(l => visitor(l.value))
+          left.foreach(l => l.parent = Some(Node(value, None, right, parent, childCount)))
+          tailRecTreeTraversal(visitor, left)
       }
     }
   }
